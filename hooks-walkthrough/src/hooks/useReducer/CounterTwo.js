92ac6b9e35f5551154b6b74e31b2a60f778542ff
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-14 11:36:45 +0530

    increment or decrement by 5 using (value:5) and action.value instead of number

--- hooks-walkthrough/src/hooks/useReducer/CounterTwo.js
+++ hooks-walkthrough/src/hooks/useReducer/CounterTwo.js
@@ -4,8 +4,8 @@ const initialState = {firstCounter:0}
 const reducer = (state,action) => {
 
     switch(action.type){
-        case 'increment': return {firstCounter: state.firstCounter+1}
-        case 'decrement' : return {firstCounter: state.firstCounter-1}
+        case 'increment': return {firstCounter: state.firstCounter+action.value}
+        case 'decrement' : return {firstCounter: state.firstCounter-action.value}
         case 'reset':   return initialState
         default: return state
     }
@@ -20,6 +20,8 @@ export default function CounterTwo() {
     <div>{count.firstCounter} </div>
     <button onClick={()=>{dispatch({type:'increment'})}} >Increment</button>
     <button onClick={()=>{dispatch({type:'decrement'})}}>decrement</button>
+    <button onClick={()=>{dispatch({type:'increment',value:5})}} >Increment by 5</button>
+    <button onClick={()=>{dispatch({type:'decrement',value:5})}}>decrement by 5</button>
     <button onClick={()=>{dispatch({type:'reset'})}}>Reset</button>
 
     </div>
