8bdf65c8504b0d69f59d3050e5b74ffc28843efe
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-14 15:21:25 +0530

    fetching data with reducer and managing the state

--- hooks-walkthrough/src/App.js
+++ hooks-walkthrough/src/App.js
@@ -1,9 +1,9 @@
 import { createContext,useReducer } from "react";
 import "./App.css";
-import DataFetchingOne from "./hooks/useReducer/DataFetchingOne";
+import DataFetchingTwo from "./hooks/useReducer/DataFetchingTwo";
 
 function App() {
-  <DataFetchingOne/>
+  <DataFetchingTwo/>
 }
 
 export default App;
